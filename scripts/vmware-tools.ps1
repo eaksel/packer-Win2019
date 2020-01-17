ef45d431d6b8f285090674cdf1c8434932c47145
eaksel <emre.aksel@ymail.com>
2020-01-17 16:22:53 +0100

    Updated VMware Tools to v11.0.5-15389592

--- scripts/vmware-tools.ps1
+++ scripts/vmware-tools.ps1
@@ -1,13 +1,13 @@
-$package = "VMware-tools-10.3.10-12406962-x86_64.exe"
-$url = "https://packages.vmware.com/tools/releases/10.3.10/windows/x64/$package"
+$package = "VMware-tools-11.0.5-15389592-x86_64.exe"
+$url = "https://packages.vmware.com/tools/releases/11.0.5/windows/x64/$package"
 
-Write-Output "***** Downloading VMWare tools"
+Write-Output "***** Downloading VMware tools"
 Invoke-WebRequest $url -UseBasicParsing -OutFile "C:\Windows\Temp\$package"
 
 $exe = "C:\Windows\Temp\$package"
 $parameters = '/S /v "/qn REBOOT=R ADDLOCAL=ALL"'
 
-Write-Output "***** Installing VMWare Guest Tools"
+Write-Output "***** Installing VMware Tools"
 Start-Process $exe $parameters -Wait
 
 Write-Output "***** Deleting $exe"
