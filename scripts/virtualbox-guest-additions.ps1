7b4d220d28e6613149586231a549f0679723b06c
eaksel <emre.aksel@ymail.com>
2020-01-17 20:25:35 +0100

    Changed virtualbox guest addition installation method to speed it up

--- scripts/virtualbox-guest-additions.ps1
+++ scripts/virtualbox-guest-additions.ps1
@@ -1,47 +1,26 @@
-# Source : https://github.com/luciusbono/Packer-Windows10/blob/master/install-guest-tools.ps1
-# Set the path of the VMWare Tools ISO - this is set in the Packer JSON file
+$ProgressPreference = "SilentlyContinue"
+$package = "VBoxGuestAdditions_6.1.2.iso"
+$url = "http://download.virtualbox.org/virtualbox/6.1.2/$package"
 
-$isopath = "C:\Windows\Temp\windows.iso"
+Write-Output "***** Oracle VM VirtualBox Guest Additions"
+Invoke-WebRequest $url -UseBasicParsing -OutFile "C:\Windows\Temp\$package"
 
-# Mount the .iso, then build the path to the installer by getting the Driveletter attribute from Get-DiskImage piped into Get-Volume and adding a :\setup.exe
-# A separate variable is used for the parameters. There are cleaner ways of doing this. I chose the /qr MSI Installer flag because I personally hate silent installers
-# Even though our build is headless. 
+$iso = "C:\Windows\Temp\$package"
 
-Write-Output "***** Mounting disk image at $isopath"
-Mount-DiskImage -ImagePath $isopath
+Write-Output "***** Mounting disk image at $iso"
+Mount-DiskImage -ImagePath $iso
 
-function virtualbox {
-
-$certdir = ((Get-DiskImage -ImagePath $isopath | Get-Volume).Driveletter + ':\cert\')
+Write-Output "***** Installing VirtualBox Certificates"
+$certdir = ((Get-DiskImage -ImagePath $iso | Get-Volume).Driveletter + ':\cert\')
 $VBoxCertUtil = ($certdir + 'VBoxCertUtil.exe')
+Get-ChildItem $certdir *.cer | ForEach-Object { & $VBoxCertUtil add-trusted-publisher $_.FullName --root $_.FullName }
 
-# Added support for VirtualBox 4.4 and above by doing this silly little trick.
-# We look for the presence of VBoxCertUtil.exe and use that as the deciding factor for what method to use.
-# The better way to do this would be to parse the Virtualbox version file that Packer can upload, but this was quick.
-
-if (Test-Path ($VBoxCertUtil)) {
-        Write-Output "Using newer (4.4 and above) certificate import method"
-	Get-ChildItem $certdir *.cer | ForEach-Object { & $VBoxCertUtil add-trusted-publisher $_.FullName --root $_.FullName}
-}
-
-else {
-        Write-Output "Using older (4.3 and below) certificate import method"
-	$certpath = ($certdir + 'oracle-vbox.cer')
-	certutil -addstore -f "TrustedPublisher" $certpath
-}
-
-$exe = ((Get-DiskImage -ImagePath $isopath | Get-Volume).Driveletter + ':\VBoxWindowsAdditions.exe')
+Write-Output "***** Installing VirtualBox Guest Additions"
+$exe = ((Get-DiskImage -ImagePath $iso | Get-Volume).Driveletter + ':\VBoxWindowsAdditions.exe')
 $parameters = '/S'
 
 Start-Process $exe $parameters -Wait
 
-}
-
-virtualbox
-
-#Time to clean up - dismount the image and delete the original ISO
-
-Write-Output "Dismounting disk image $isopath"
-Dismount-DiskImage -ImagePath $isopath
-Write-Output "Deleting $isopath"
-Remove-Item $isopath
+Write-Output "***** Dismounting & Deleting $iso"
+Dismount-DiskImage -ImagePath $iso
+Remove-Item $iso
