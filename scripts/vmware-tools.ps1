96d1553cad88cc15e053b4b9f971b7e9cc26f55b
eaksel <emre.aksel@ymail.com>
2020-09-05 20:23:14 +0200

    Update to always download the latest version of VMware Tools

--- scripts/vmware-tools.ps1
+++ scripts/vmware-tools.ps1
@@ -1,11 +1,16 @@
 $ProgressPreference = "SilentlyContinue"
-$package = "VMware-tools-11.0.5-15389592-x86_64.exe"
-$url = "https://packages.vmware.com/tools/releases/11.0.5/windows/x64/$package"
+
+$webclient = New-Object System.Net.WebClient
+$version_url = "https://packages.vmware.com/tools/releases/latest/windows/x64/"
+$raw_package = $webclient.DownloadString($version_url)
+$raw_package -match "VMware-tools[\w-\d\.]*\.exe"
+$package = $Matches.0
+
+$url = "https://packages.vmware.com/tools/releases/latest/windows/x64/$package"
 $exe = "$Env:TEMP\$package"
 
 Write-Output "***** Downloading VMware Tools"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $exe)
+$webclient.DownloadFile($url, $exe)
 
 $parameters = '/S /v "/qn REBOOT=R ADDLOCAL=ALL"'
 
